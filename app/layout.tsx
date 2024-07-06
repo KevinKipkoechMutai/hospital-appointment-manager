4e2327156f178645185cea9c222620a187838746
kevinmutai <mutaikevinkip.05@gmail.com>
2024-07-06 23:04:57 +0300

    format home layout classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antaliased')}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antaliased', fontSans.variable)}>{children}</body>
     </html>
   );
 }
